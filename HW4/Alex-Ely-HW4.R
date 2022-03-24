b1d731093a006558f62c5644a9b6cda9c4097bcf
Alex Ely <ely.m.alexander@gmail.com>
2022-03-24 17:51:07 -0400

    Update Alex-Ely-HW4.R

--- HW4/Alex-Ely-HW4.R
+++ HW4/Alex-Ely-HW4.R
@@ -1,6 +1,60 @@
 #Alex Ely
 #Homework 4
 
+#1___________________________________________________________________________
+#a)
+#calculating point estimate
+realVal = c(334,369,191,63,22,12,9,0,0,0,0)
+pEst = sum(realVal*0:10)/10000
+
+#b)
+#calculating expected value
+expVal = dbinom(0:10,10,pEst)*1000
+
+#c)
+#counting expected values less than 5
+count = 0
+for(i in 1:11) {
+  if(expVal[i] < 5)
+    count = count + expVal[i]
+}
+
+#d)
+#computing chisquared
+chiSq <- sum((realVal - expVal)^2/expVal)
+
+#e)
+#computing p value
+pVal = 1-pchisq(chiSq,10)
+
+
+#2___________________________________________________________________________
+#a)
+#H0: 2 variables, independent for education and location)
+#Ha: 2 variables, independent for education and location
+
+#b)
+#creating observed matrix
+obsMtx <- matrix(c(15,8,6,12,15,8,8,9,7),3,3)
+
+#c)
+#generating expected matrix
+expMtx <- chisq.test(obsMtx)$expected
+
+#d)
+#statistic value of chisquare test
+stat <- chisq.test(obsMtx)$statistic
+
+#e)
+qchisq(1-0.05,4)
+
+#f)
+p <- chisq.test(obsMtx)$p.value
+
+#g)
+#We did not reject the null hypothesis
+
+
 #3___________________________________________________________________________
 #a)
 #setting working directory
